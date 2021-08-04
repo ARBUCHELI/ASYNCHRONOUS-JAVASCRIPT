dedde3b994f37048ccd6392990804dd2421fc0da
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-08-04 14:34:49 +0300

    Create app.js

---
+++ PROMISES/The Node setTimeout() Function/app.js
@@ -0,0 +1,19 @@
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+const usingSTO = () => {
+  console.log('This is a test');
+}
+
+setTimeout(usingSTO, 2500);
+
+
+
+
+
+
+
+
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
