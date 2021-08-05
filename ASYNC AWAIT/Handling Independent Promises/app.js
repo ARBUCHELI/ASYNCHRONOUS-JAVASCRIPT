adddda0ec32ad1d605f6783700cdf9c8d3c762cf
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-08-05 10:17:34 +0300

    Create app.js

---
+++ ASYNC AWAIT/Handling Independent Promises/app.js
@@ -0,0 +1,13 @@
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
+
+// Write your code below:
+const serveDinner = async() => {
+  const vegetablePromise = steamBroccoli();
+  const starchPromise = cookRice();
+  const proteinPromise = bakeChicken();
+  const sidePromise = cookBeans();
+
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`);
+}
+
+serveDinner();
