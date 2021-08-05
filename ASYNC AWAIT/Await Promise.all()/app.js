bf01e81d519ce788376e6a920d1c399b8de10e2a
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-08-05 10:34:26 +0300

    Create app.js

---
+++ ASYNC AWAIT/Await Promise.all()/app.js
@@ -0,0 +1,10 @@
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
+
+// Write your code below:
+const serveDinnerAgain = async () => {
+  const foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]);
+
+  console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`)
+}
+
+serveDinnerAgain();
